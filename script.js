ad2dc18bfbce8d19c164beb2c5173b48f263de50
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-06-15 23:19:49 -0400

    se añade evento resert() para dejar formulario en blanco una vez sea enviada la informacion

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-// JavaScript
 class Cliente {
-    constructor(nombre, apellido, edad) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.edad = edad;
-      //this.opciones = opciones;
-    }
+  constructor(nombre, apellido, edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    //this.opciones = opciones;
   }
-  
-  // Obtener referencia al formulario
-  const formulario = document.getElementById('formularioSuscripcion');
-  
-  // Manejador de evento para el envío del formulario
-  formulario.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar que se envíe el formulario
-    
-    // Obtener los valores ingresados por el usuario
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const edad = parseInt(document.getElementById('edad').value);
-    
-    // Crear un objeto de Persona con los valores ingresados
-    const persona = new Cliente(nombre, apellido, edad);
-    
-    // Utilizar el objeto creado
-    console.log(persona);
-  });
-  
+}
+
+// Obtener referencia al formulario
+const formulario = document.getElementById('formularioSuscripcion');
+
+// Manejador de evento para el envío del formulario
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar que se envíe el formulario
+  //formulario.reset(); // Reiniciar el formulario a su estado predeterminado
+
+  // Obtener los valores ingresados por el usuario
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const edad = parseInt(document.getElementById('edad').value);
+
+  // Crear un objeto de Cliente con los valores ingresados
+  const cliente = new Cliente(nombre, apellido, edad);
+
+  // Utilizar el objeto creado
+  console.log(cliente);
+});
+
+
