9d80e114af94230dd285c4be95d4ff379f1805d4
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-06-16 23:38:06 -0400

    se introdujo la selección de opciones de tipo de suscripción de manera dinámica usando un bucle for iterando sobre un array y se creo un condicional if para filtrar la edad del cliente y mostrar un mensaje en pantalla dependiendo de la edad del cliente al momento de la suscripción

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class Cliente {
+/*class Cliente {
   constructor(nombre, apellido, edad) {
     this.nombre = nombre;
     this.apellido = apellido;
@@ -23,11 +23,83 @@ formulario.addEventListener('submit', function(event) {
   // Crear un objeto de Cliente con los valores ingresados
   const cliente = new Cliente(nombre, apellido, edad);
 
+
   // Utilizar el objeto creado
   console.log(cliente);
 
   formulario.reset(); // Reiniciar el formulario a su estado predeterminado
+});*/
+
+// Obtener referencia al formulario
+const formulario = document.getElementById('formularioSuscripcion');
+
+// Obtener referencia al select de opciones
+const opcionesSelect = document.getElementById('opciones');
+
+// Crear un array de opciones
+const opciones = ['Legionario', 'Espartano', 'Tercio', 'Navy Seal'];
+
+// Generar las opciones dinámicamente
+for (let i = 0; i < opciones.length; i++) {
+  const opcion = document.createElement('option');
+  opcion.value = opciones[i];
+  opcion.textContent = opciones[i];
+  opcionesSelect.appendChild(opcion);
+}
+
+// Manejador de evento para el envío del formulario
+formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar que se envíe el formulario
+
+  class Cliente {
+    constructor(nombre, apellido, edad, opcionCliente) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.opcionCliente = opcionCliente;
+    }
+  }
+
+  // Obtener los valores ingresados por el usuario
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const edad = parseInt(document.getElementById('edad').value);
+  const opcionCliente = opcionesSelect.value;
+
+  //MENSAJE DE ERROR EN CASO DE SER MENOR DE EDAD
+  // Obtener referencia al elemento contenedor donde se mostrará el mensaje
+const contenedorMensaje = document.getElementById('contenedor-mensaje');
+
+// Verificar si la edad es menor de 18 años
+if (edad < 18) {
+  // Crear un nuevo elemento de párrafo
+  const mensajeElemento = document.createElement('p');
+
+  // Establecer el contenido de texto del mensaje
+  mensajeElemento.textContent = 'Debes ser mayor de 18 años para acceder.';
+
+  // Agregar el elemento al contenedor
+  contenedorMensaje.appendChild(mensajeElemento);
+}else{
+  // Crear un nuevo elemento de párrafo
+  const mensajeElemento = document.createElement('p');
+
+  // Establecer el contenido de texto del mensaje
+  mensajeElemento.textContent = '¡Bienvenido al Warriors Box, futuro guerrero!';
+
+  // Agregar el elemento al contenedor
+  contenedorMensaje.appendChild(mensajeElemento);
+}
+
+
+  // Realizar acciones con los valores ingresados
+  const cliente = new Cliente(nombre, apellido, edad, opcionCliente);
+
+  console.log(cliente);
+
+  formulario.reset(); // Reiniciar el formulario a su estado predeterminado
 });
+
 
 
 
