e241592256225b5926af1079667e8f7170382d1b
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-06-17 23:43:30 -0400

    se añadió funcionalidad para indentificar la edad del cliente mostrar mensaje de suscripcion en verde o rojo dependiendo de si es mayor de edad o no. ademas se incluye funcionalidad en JS para imprimir en consola el objeto cliente solo cuando el cliente es mayor de edad

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-/*class Cliente {
-  constructor(nombre, apellido, edad) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-    //this.opciones = opciones;
-  }
-}
-
-// Obtener referencia al formulario
-const formulario = document.getElementById('formularioSuscripcion');
-
-// Manejador de evento para el envío del formulario
-formulario.addEventListener('submit', function(event) {
-  event.preventDefault(); // Evitar que se envíe el formulario
-  
-
-  // Obtener los valores ingresados por el usuario
-  const nombre = document.getElementById('nombre').value;
-  const apellido = document.getElementById('apellido').value;
-  const edad = parseInt(document.getElementById('edad').value);
-
-  // Crear un objeto de Cliente con los valores ingresados
-  const cliente = new Cliente(nombre, apellido, edad);
-
-
-  // Utilizar el objeto creado
-  console.log(cliente);
-
-  formulario.reset(); // Reiniciar el formulario a su estado predeterminado
-});*/
-
 // Obtener referencia al formulario
 const formulario = document.getElementById('formularioSuscripcion');
 
@@ -68,34 +36,44 @@ formulario.addEventListener('submit', function(event) {
 
   //MENSAJE DE ERROR EN CASO DE SER MENOR DE EDAD
   // Obtener referencia al elemento contenedor donde se mostrará el mensaje
-const contenedorMensaje = document.getElementById('contenedor-mensaje');
+  const contenedorMensaje = document.getElementById('contenedor-mensaje');
 
-// Verificar si la edad es menor de 18 años
-if (edad < 18) {
+  // Verificar si la edad es menor de 18 años
+  if (edad < 18) {
   // Crear un nuevo elemento de párrafo
   const mensajeElemento = document.createElement('p');
 
   // Establecer el contenido de texto del mensaje
   mensajeElemento.textContent = 'Debes ser mayor de 18 años para acceder.';
 
+  // Establecer el color del texto
+  mensajeElemento.style.color = 'red';
+
+  contenedorMensaje.innerHTML = '';
+
   // Agregar el elemento al contenedor
   contenedorMensaje.appendChild(mensajeElemento);
-}else{
+
+  }else{
   // Crear un nuevo elemento de párrafo
   const mensajeElemento = document.createElement('p');
 
   // Establecer el contenido de texto del mensaje
   mensajeElemento.textContent = '¡Bienvenido al Warriors Box, futuro guerrero!';
 
+  // Establecer el color del texto
+  mensajeElemento.style.color = '#53D106';
+
+  contenedorMensaje.innerHTML = '';
+
   // Agregar el elemento al contenedor
   contenedorMensaje.appendChild(mensajeElemento);
-}
-
 
   // Realizar acciones con los valores ingresados
   const cliente = new Cliente(nombre, apellido, edad, opcionCliente);
 
   console.log(cliente);
+  }
 
   formulario.reset(); // Reiniciar el formulario a su estado predeterminado
 });
